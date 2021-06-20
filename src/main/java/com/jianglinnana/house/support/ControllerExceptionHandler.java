efdfd391bce7811821883a738049ab81f1b35774
jianglinnana <jianglinnana@163.com>
2021-06-20 11:08:27 +0800

    全局异常拦截

--- src/main/java/com/jianglinnana/house/support/ControllerExceptionHandler.java
+++ src/main/java/com/jianglinnana/house/support/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.jianglinnana.house.support;
 
+import com.jianglinnana.house.exception.HouseApiException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -7,6 +9,19 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
+    /**
+     * 系统异常处理器
+     *
+     * @param e 系统异常
+     * @return 错误包装信息
+     */
+    @ExceptionHandler(HouseApiException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponsePackage houseApiExceptionHandler(HouseApiException e){
+        ErrorMessage errorMessage = new ErrorMessage(e.getErrorCode(), e.getMessage(), e.getUserPromptMsg());
+        return ResponsePackage.fail(errorMessage);
+    }
     /**
      * 通用异常处理器
      *
@@ -14,8 +29,10 @@ public class ControllerExceptionHandler {
      * @return 错误包装信息
      */
     @ExceptionHandler(Exception.class)
-    public ResponsePackage handleGlobalException(){
-        System.out.println("haha");
-        return ResponsePackage.fail(new ErrorMessage(ErrorCode.A0001,"sa","asd"));
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponsePackage handleGlobalException(Exception e){
+        e.printStackTrace();
+        return ResponsePackage.fail(new ErrorMessage(ErrorCode.A0001,
+                e.getMessage(),"诶呀～服务器开了个小差 X﹏X"));
     }
 }
--- src/main/java/com/jianglinnana/house/support/ErrorCode.java
+++ src/main/java/com/jianglinnana/house/support/ErrorCode.java
@@ -7,6 +7,12 @@ public enum ErrorCode {
     OK("成功"),
 
     A0001("用户端错误"),
+
+    A0201("用户未登录"),
+
+    A0203("用户不存在"),
+
+    B0001("服务端错误"),
     ;
 
     private final String description;
