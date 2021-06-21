173fb036283fc28ed5c10275d0f90c2436a597a4
jianglinnana <jianglinnana@163.com>
2021-06-21 11:36:29 +0800

    实现mybatis代码生成器

--- src/test/java/com/jianglinnana/house/HouseApplicationTests.java
+++ src/test/java/com/jianglinnana/house/HouseApplicationTests.java
@@ -1,5 +1,14 @@
 package com.jianglinnana.house;
 
+//import com.baomidou.mybatisplus.annotation.DbType;
+//import com.baomidou.mybatisplus.annotation.IdType;
+//import com.baomidou.mybatisplus.generator.AutoGenerator;
+//import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+//import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+//import com.baomidou.mybatisplus.generator.config.PackageConfig;
+//import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+//import com.baomidou.mybatisplus.generator.config.rules.DateType;
+//import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +17,64 @@ class HouseApplicationTests {
 
 	@Test
 	void contextLoads() {
+//        GlobalConfig config = new GlobalConfig();
+//        config.setOpen(false)
+//                .setAuthor("jianglinnana") // 作者
+//                .setOutputDir("C:\\Users\\jianglinnana\\Desktop\\house\\src\\main\\java") // 生成路径
+//                .setFileOverride(true)  // 文件覆盖
+//                .setIdType(IdType.ASSIGN_ID) // 主键策略 数字id
+//                .setServiceName("%sService")  // 设置生成的service接口的名
+//                .setMapperName("%sMapper")
+//                .setServiceImplName("%sServiceImpl")
+//                .setControllerName("%sController")
+//                .setSwagger2(true)
+//                .setBaseResultMap(true)
+//                .setBaseColumnList(true)
+//                .setXmlName("%sMapper")
+//                .setDateType(DateType.TIME_PACK);
+//
+//        //2. 数据源配置
+//        DataSourceConfig dsConfig = new DataSourceConfig();
+//        dsConfig.setDbType(DbType.MYSQL)  // 设置数据库类型
+//                .setDriverName("com.mysql.cj.jdbc.Driver")
+//                .setUrl("jdbc:mysql://192.168.2.211:3306/vote?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf8&useSSL=true")
+//                .setUsername("root")
+//                .setPassword("123456");
+//
+//        //3. 策略配置
+//        StrategyConfig stConfig = new StrategyConfig();
+//        stConfig.setCapitalMode(true) //全局大写命名
+//                .setLogicDeleteFieldName("deleted")
+//                //NamingStrategy.underline_to_camel下划线转驼峰命名  user_id -> userId
+//                .setNaming(NamingStrategy.underline_to_camel)
+//                .setEntityTableFieldAnnotationEnable(true)//开启实体注解
+//                .setRestControllerStyle(true)//rest风格controller
+//                .setChainModel(true)//链式setter
+////                .setSuperControllerClass(BaseController.class)//父类
+////                .setSuperEntityClass(BaseEntity.class)//父类
+//                .setControllerMappingHyphenStyle(true)//？
+//                .setTablePrefix("t_");//表名前缀
+//
+//        //4. 包名策略配置
+//        PackageConfig pkConfig = new PackageConfig();
+//        pkConfig.setParent("com.jianglinnana.house.test")
+//                .setMapper("mapper")
+//                .setService("service")
+//                .setServiceImpl("service.impl")
+//                .setController("controller")
+//                .setEntity("entity")
+//                .setXml("mapper.xml");
+//
+//        //5. 整合配置
+//        AutoGenerator ag = new AutoGenerator();
+//
+//        ag.setGlobalConfig(config)
+//                .setDataSource(dsConfig)
+//                .setStrategy(stConfig)
+//                .setPackageInfo(pkConfig);
+//
+//        //6. 执行
+//        ag.execute();
 	}
 
 }
