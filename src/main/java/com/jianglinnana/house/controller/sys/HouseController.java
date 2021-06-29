0a77b301959b1a0d34d4cc80dad822b4dda4a96d
jianglinnana <jianglinnana@163.com>
2021-06-29 15:12:45 +0800

    6-29 完成房屋详情页面

--- src/main/java/com/jianglinnana/house/controller/sys/HouseController.java
+++ src/main/java/com/jianglinnana/house/controller/sys/HouseController.java
@@ -3,6 +3,8 @@ package com.jianglinnana.house.controller.sys;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.jianglinnana.house.model.entity.sys.House;
+import com.jianglinnana.house.model.entity.sys.HouseDetail;
+import com.jianglinnana.house.service.sys.HouseDetailService;
 import com.jianglinnana.house.service.sys.HouseService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,24 +32,32 @@ public class HouseController {
     @Autowired
     private HouseService houseService;
 
+    @Autowired
+    private HouseDetailService houseDetailService;
+
     @RequestMapping("listhouse")
     public String listHouse(Model model) {
         List<House> list = houseService.list();
-        model.addAttribute("house",list);
+        model.addAttribute("house", list);
         return "index";
     }
 
     @RequestMapping("/{houseId:\\d+}")
-    public String housePage(Model model,@PathVariable("houseId") Integer houseId){
+    public String housePage(Model model, @PathVariable("houseId") Integer houseId) {
         House house = houseService.getById(houseId);
-        model.addAttribute("houseId",house);
+        model.addAttribute("houseId", house);
+        HouseDetail houseDetail = houseDetailService
+                .getOne(new QueryWrapper<HouseDetail>().lambda()
+                        .eq(HouseDetail::getHouseId, house.getId())
+                        .last("LIMIT 1"));
+        model.addAttribute("houseDetail", houseDetail);
         return "houseDatil";
     }
 
     @RequestMapping("/select")
-    public String selectHouseByName(Model model,@RequestParam(value = "info") String info){
+    public String selectHouseByName(Model model, @RequestParam(value = "info") String info) {
         List<House> list = houseService.list(new QueryWrapper<House>().lambda().like(House::getTitle, info));
-        model.addAttribute("houseByName",list);
+        model.addAttribute("houseByName", list);
         return "house";
     }
 
